aa66479050c095beb5a73a7518fb1010bf752ac6
Ransmillir Gabriel <rgjt1@aluno.ifal.edu.br>
2026-05-28 17:44:54 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,14 @@
+const prompt = require("prompt-sync")();
+let n = prompt("Qual seu nome?: ");
+
+if (n === "Nickie") {
+    console.log("OIIII NICKIEEE");
+} if (n === "Mandy") {
+    console.log("Oiiii");
+} if (n === "Jorge") {
+    console.log("Nickie...?")
+} if (n === "Hide") {
+   console.log("Salve Hide") 
+} if (n === "Aster") {
+    console.log("OIIII MEU ANJO")
+}
